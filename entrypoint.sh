7bf6128da005d992ecdc2800781105343b1df111
Jabi-james <91572300+Jabi-james@users.noreply.github.com>
2025-04-08 02:29:51 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,7 @@ set_domain_dir() {
         fi
         return 0
     else
-        echo "正在检测 NodeJS 环境，请稍候..."
+        echo "Detecting NodeJS environment, please wait..."
         nohup devil www del ${DOMAIN} >/dev/null 2>&1
         devil www add ${DOMAIN} nodejs /usr/local/bin/node22
         rm -rf ${WORKDIR}/public
@@ -40,14 +40,14 @@ reserve_port() {
     add_port() {
         local port=$1
         local result=$(devil port add tcp "$port")
-        echo "尝试添加预留端口 $port: $result" 
+        echo "Try adding a reserved port $port: $result" 
     }
 
     local delete_udp_port
     delete_udp_port() {
         local port=$1
         local result=$(devil port del udp "$port")
-        echo "删除 UDP 端口 $port: $result"
+        echo "Deleting a UDP Port $port: $result"
     }
 
     update_port_list() {
@@ -55,7 +55,7 @@ reserve_port() {
         port_count=$(echo "$port_list" | grep -c 'tcp')
     }
 
-    # 循环删除 UDP 端口
+    # Loop through UDP ports
     port_list=$(devil port list)
     while echo "$port_list" | grep -q 'udp'; do
         UDP_PORT=$(echo "$port_list" | grep 'udp' | awk 'NR==1{print $1}')
@@ -65,8 +65,8 @@ reserve_port() {
 
     update_port_list
 
-    # 随机选择起始端口
-    start_port=$(( RANDOM % 63077 + 1024 ))  # 1024-64000之间的随机数
+    # Randomly select the starting port
+    start_port=$(( RANDOM % 63077 + 1024 ))  # 1024-64000Random number between
 
     if [ $start_port -le 32512 ]; then
         current_port=$start_port
@@ -83,7 +83,7 @@ reserve_port() {
         PORT1=$(echo "$port_list" | grep 'tcp' | awk 'NR==1{print $1}')
         PORT2=$(echo "$port_list" | grep 'tcp' | awk 'NR==2{print $1}')
         PORT3=$(echo "$port_list" | grep 'tcp' | awk 'NR==3{print $1}')
-        echo "预留端口为 $PORT1 $PORT2 $PORT3"
+        echo "Reserved ports for$PORT1 $PORT2 $PORT3"
         return 0
     else
         needed_ports=$((3 - port_count))
@@ -101,7 +101,7 @@ reserve_port() {
             attempts=$((attempts + 1))
 
             if [ $attempts -ge $max_attempts ]; then
-                echo "超过最大尝试次数，无法添加足够的预留端口"
+                echo "Exceeded the maximum number of attempts and could not add enough reserved ports"
                 exit 1
             fi
         done
@@ -111,7 +111,7 @@ reserve_port() {
     PORT1=$(echo "$port_list" | grep 'tcp' | awk 'NR==1{print $1}')
     PORT2=$(echo "$port_list" | grep 'tcp' | awk 'NR==2{print $1}')
     PORT3=$(echo "$port_list" | grep 'tcp' | awk 'NR==3{print $1}')
-    echo "预留端口为 $PORT1 $PORT2 $PORT3"
+    echo "Reserved ports for $PORT1 $PORT2 $PORT3"
 }
 
 
@@ -127,38 +127,38 @@ generate_dotenv() {
     echo "$uuid"
     }
 
-    printf "请输入 ARGO_AUTH（必填）："
+    printf "Please enter ARGO_AUTH (required）："
     read -r ARGO_AUTH
-    printf "请输入 ARGO_DOMAIN_VL（必填）："
+    printf "Please enter ARGO_DOMAIN_VL (required）："
     read -r ARGO_DOMAIN_VL
-    echo "请在Cloudflare中为隧道添加域名 ${ARGO_DOMAIN_VL} 指向 HTTP://localhost:${PORT1},添加完成请按回车继续"
+    echo "Please add the domain name for the tunnel in Cloudflare ${ARGO_DOMAIN_VL} Point to HTTP://localhost:${PORT1}, Press Enter to continue"
     read
-    printf "请输入 ARGO_DOMAIN_VM（必填）："
+    printf "Please enter ARGO_DOMAIN_VM (required）："
     read -r ARGO_DOMAIN_VM
-    echo "请在Cloudflare中为隧道添加域名 ${ARGO_DOMAIN_VM} 指向 HTTP://localhost:${PORT2},添加完成请按回车继续"
+    echo "Please add the domain name for the tunnel in Cloudflare ${ARGO_DOMAIN_VM} Point to HTTP://localhost:${PORT2}, Press Enter to continue"
     read
     printf "请输入 ARGO_DOMAIN_TR（必填）："
     read -r ARGO_DOMAIN_TR
-    echo "请在Cloudflare中为隧道添加域名 ${ARGO_DOMAIN_TR} 指向 HTTP://localhost:${PORT3},添加完成请按回车继续"
+    echo "Please add the domain name for the tunnel in Cloudflare ${ARGO_DOMAIN_TR} Point to HTTP://localhost:${PORT3}, Press Enter to continue"
     read
-    printf "请输入 UUID（默认值：de04add9-5c68-8bab-950c-08cd5320df18）："
+    printf "Please enter the UUID (default：de04add9-5c68-8bab-950c-08cd5320df18）："
     read -r UUID
-    printf "请输入 WSPATH（默认值：serv00）："
+    printf "Please enter WSPATH (the default value：serv00）："
     read -r WSPATH
-    printf "请输入 WEB_USERNAME（默认值：admin）："
+    printf "Please enter WEB_USERNAME (default：admin）："
     read -r WEB_USERNAME
-    printf "请输入 WEB_PASSWORD（默认值：password）："
+    printf "Please enter WEB_PASSWORD (default：password）："
     read -r WEB_PASSWORD
 
     if [ -z "${ARGO_AUTH}" ] || [ -z "${ARGO_DOMAIN_VL}" ] || [ -z "${ARGO_DOMAIN_VM}" ] || [ -z "${ARGO_DOMAIN_TR}" ]; then
-    echo "Error! 所有选项都不能为空！"
+    echo "Error! All options cannot be empty！"
     rm -rf ${WORKDIR}/*
     rm -rf ${WORKDIR}/.*
     exit 1
     fi
 
     if [ -z "${UUID}" ]; then
-        echo "正在生成 UUID..."
+        echo "Generating UUID..."
         UUID=$(generate_uuid)
     fi
     if [ -z "${WSPATH}" ]; then
@@ -184,30 +184,30 @@ EOF
 }
 
 get_app() {
-    echo "正在下载 app.js 请稍候..."
+    echo "Downloading app.js Please wait..."
     wget -t 10 -qO ${WORKDIR}/app.js https://raw.githubusercontent.com/k0baya/X-for-serv00/main/app.js
     if [ $? -ne 0 ]; then
-        echo "app.js 下载失败！请检查网络情况！"
+        echo "app.js Download failed! Please check the network status！"
         exit 1
     fi
-    echo "正在下载 package.json 请稍候..."
+    echo "Downloading package.json Please wait..."
     wget -t 10 -qO ${WORKDIR}/package.json https://raw.githubusercontent.com/k0baya/X-for-serv00/main/package.json
     if [ $? -ne 0 ]; then
-        echo "package.json 下载失败！请检查网络情况！"
+        echo "package.json Download failed! Please check the network status！"
         exit 1
     fi
 
-    echo "正在安装依赖..."
+    echo "Installing dependencies..."
     nohup npm22 install > /dev/null 2>&1
 }
 
 get_core() {
     local TMP_DIRECTORY=$(mktemp -d)
     local ZIP_FILE="${TMP_DIRECTORY}/Xray-freebsd-64.zip"
-    echo "正在下载 Web.js 请稍候..."
+    echo "Downloading Web.js Please wait..."
     wget -t 10 -qO "$ZIP_FILE" https://github.com/XTLS/Xray-core/releases/latest/download/Xray-freebsd-64.zip
     if [ $? -ne 0 ]; then
-        echo "Web.js 安装失败！请检查网络情况！"
+        echo "Web.js Installation failed! Please check the network status！"
         exit 1
     else
         unzip -qo "$ZIP_FILE" -d "$TMP_DIRECTORY"
@@ -215,17 +215,17 @@ get_core() {
         rm -rf "$TMP_DIRECTORY"
     fi
     
-    echo "正在下载 GEOSITE 数据库，请稍候..."
+    echo "Downloading GEOSITE database, please wait..."
     wget -t 10 -qO ${WORKDIR}/geosite.dat https://github.com/v2fly/domain-list-community/releases/latest/download/dlc.dat
     if [ $? -ne 0 ]; then
-        echo "GEOSITE 数据库下载失败！请检查网络情况！"
+        echo "GEOSITE database download failed! Please check the network status！"
         exit 1
     fi
         
-    echo "正在下载 GEOIP 数据库，请稍候..."
+    echo "Downloading GEOIP database, please wait..."
     wget -t 10 -qO ${WORKDIR}/geoip.dat https://github.com/v2fly/geoip/releases/latest/download/geoip.dat
     if [ $? -ne 0 ]; then
-        echo "GEOIP 数据库下载失败！请检查网络情况！"
+        echo "GEOIP Database download failed! Please check the network status！"
         exit 1
     fi
 }
@@ -338,7 +338,7 @@ check_file() {
     wget -t 10 https://cloudflared.bowring.uk/binaries/cloudflared-freebsd-latest.7z
 
     if [ \$? -ne 0 ]; then
-        echo "Cloudflared 客户端安装失败！请检查 hosts 文件是否屏蔽了下载地址！" > list
+        echo "Cloudflared Client installation failed! Please check if the hosts file blocks the download address！" > list
         exit 1
     else
         7z x cloudflared-freebsd-latest.7z -bb > /dev/null \
@@ -375,7 +375,7 @@ EOF
             nohup ./cloudflared tunnel --edge-ip-version auto --protocol http2 run --token \${ARGO_AUTH} > /dev/null 2>&1 &
         fi
     else
-        echo '请设置环境变量 \$ARGO_AUTH 和 \$ARGO_DOMAIN_TR、\$ARGO_DOMAIN_VL、\$ARGO_DOMAIN_VM' > \${WORKDIR}/list
+        echo 'Please set the environment variable \$ARGO_AUTH and \$ARGO_DOMAIN_TR、\$ARGO_DOMAIN_VL、\$ARGO_DOMAIN_VM' > \${WORKDIR}/list
         exit 1
     fi
     }
@@ -386,35 +386,35 @@ export_list() {
 *******************************************
 V2-rayN:
 ----------------------------
-vless://${UUID}@upos-sz-mirrorcf1ov.bilivideo.com:443?path=%2F${WSPATH}-vless%3Fed%3D2560&security=tls&encryption=none&host=\${ARGO_DOMAIN_VL}&type=ws&sni=\${ARGO_DOMAIN_VL}#Argo-k0baya-Vless
+vless://${UUID}@upos-sz-mirrorcf1ov.bilivideo.com:443?path=%2F${WSPATH}-vless%3Fed%3D2560&security=tls&encryption=none&host=\${ARGO_DOMAIN_VL}&type=ws&sni=\${ARGO_DOMAIN_VL}#Argo-Vless
 ----------------------------
 vmess://\$(echo \$VMESS | base64 | tr -d '\n')
 ----------------------------
-trojan://${UUID}@upos-sz-mirrorcf1ov.bilivideo.com:443?path=%2F${WSPATH}-trojan%3Fed%3D2560&security=tls&host=\${ARGO_DOMAIN_TR}&type=ws&sni=\${ARGO_DOMAIN_TR}#Argo-k0baya-Trojan
+trojan://${UUID}@upos-sz-mirrorcf1ov.bilivideo.com:443?path=%2F${WSPATH}-trojan%3Fed%3D2560&security=tls&host=\${ARGO_DOMAIN_TR}&type=ws&sni=\${ARGO_DOMAIN_TR}#Argo-Trojan
 *******************************************
-小火箭:
+Little Rocket:
 ----------------------------
-vless://${UUID}@upos-sz-mirrorcf1ov.bilivideo.com:443?encryption=none&security=tls&type=ws&host=\${ARGO_DOMAIN_VL}&path=/${WSPATH}-vless?ed=2560&sni=\${ARGO_DOMAIN_VL}#Argo-k0baya-Vless
+vless://${UUID}@upos-sz-mirrorcf1ov.bilivideo.com:443?encryption=none&security=tls&type=ws&host=\${ARGO_DOMAIN_VL}&path=/${WSPATH}-vless?ed=2560&sni=\${ARGO_DOMAIN_VL}#Argo-Vless
 ----------------------------
 vmess://$(echo "none:${UUID}@upos-sz-mirrorcf1ov.bilivideo.com:443" | base64 | tr -d '\n')?remarks=Argo-k0baya-Vmess&obfsParam=\${ARGO_DOMAIN_VM}&path=/${WSPATH}-vmess?ed=2560&obfs=websocket&tls=1&peer=\${ARGO_DOMAIN_VM}&alterId=0
 ----------------------------
-trojan://${UUID}@upos-sz-mirrorcf1ov.bilivideo.com:443?peer=\${ARGO_DOMAIN_TR}&plugin=obfs-local;obfs=websocket;obfs-host=\${ARGO_DOMAIN_TR};obfs-uri=/${WSPATH}-trojan?ed=2560#Argo-k0baya-Trojan
+trojan://${UUID}@upos-sz-mirrorcf1ov.bilivideo.com:443?peer=\${ARGO_DOMAIN_TR}&plugin=obfs-local;obfs=websocket;obfs-host=\${ARGO_DOMAIN_TR};obfs-uri=/${WSPATH}-trojan?ed=2560#Argo-Trojan
 *******************************************
 Clash:
 ----------------------------
-- {name: Argo-k0baya-Vless, type: vless, server: upos-sz-mirrorcf1ov.bilivideo.com, port: 443, uuid: ${UUID}, tls: true, servername: \${ARGO_DOMAIN_VL}, skip-cert-verify: false, network: ws, ws-opts: {path: /${WSPATH}-vless?ed=2560, headers: { Host: \${ARGO_DOMAIN_VL}}}, udp: true}
+- {name: Argo-Vless, type: vless, server: upos-sz-mirrorcf1ov.bilivideo.com, port: 443, uuid: ${UUID}, tls: true, servername: \${ARGO_DOMAIN_VL}, skip-cert-verify: false, network: ws, ws-opts: {path: /${WSPATH}-vless?ed=2560, headers: { Host: \${ARGO_DOMAIN_VL}}}, udp: true}
 ----------------------------
-- {name: Argo-k0baya-Vmess, type: vmess, server: upos-sz-mirrorcf1ov.bilivideo.com, port: 443, uuid: ${UUID}, alterId: 0, cipher: none, tls: true, skip-cert-verify: true, network: ws, ws-opts: {path: /${WSPATH}-vmess?ed=2560, headers: {Host: \${ARGO_DOMAIN_VM}}}, udp: true}
+- {name: Argo-Vmess, type: vmess, server: upos-sz-mirrorcf1ov.bilivideo.com, port: 443, uuid: ${UUID}, alterId: 0, cipher: none, tls: true, skip-cert-verify: true, network: ws, ws-opts: {path: /${WSPATH}-vmess?ed=2560, headers: {Host: \${ARGO_DOMAIN_VM}}}, udp: true}
 ----------------------------
-- {name: Argo-k0baya-Trojan, type: trojan, server: upos-sz-mirrorcf1ov.bilivideo.com, port: 443, password: ${UUID}, udp: true, tls: true, sni: \${ARGO_DOMAIN_TR}, skip-cert-verify: false, network: ws, ws-opts: { path: /${WSPATH}-trojan?ed=2560, headers: { Host: \${ARGO_DOMAIN_TR} } } }
+- {name: Argo-Trojan, type: trojan, server: upos-sz-mirrorcf1ov.bilivideo.com, port: 443, password: ${UUID}, udp: true, tls: true, sni: \${ARGO_DOMAIN_TR}, skip-cert-verify: false, network: ws, ws-opts: { path: /${WSPATH}-trojan?ed=2560, headers: { Host: \${ARGO_DOMAIN_TR} } } }
 *******************************************
 EOF
 
-echo \$(echo -n "vless://${UUID}@upos-sz-mirrorcf1ov.bilivideo.com:443?path=%2F${WSPATH}-vless%3Fed%3D2560&security=tls&encryption=none&host=\${ARGO_DOMAIN_VL}&type=ws&sni=\${ARGO_DOMAIN_VL}#Argo-k0baya-Vless
+echo \$(echo -n "vless://${UUID}@upos-sz-mirrorcf1ov.bilivideo.com:443?path=%2F${WSPATH}-vless%3Fed%3D2560&security=tls&encryption=none&host=\${ARGO_DOMAIN_VL}&type=ws&sni=\${ARGO_DOMAIN_VL}#Argo-Vless
 
 vmess://\$(echo \$VMESS | base64 | tr -d '\n')
 
-trojan://${UUID}@upos-sz-mirrorcf1ov.bilivideo.com:443?path=%2F${WSPATH}-trojan%3Fed%3D2560&security=tls&host=\${ARGO_DOMAIN_TR}&type=ws&sni=\${ARGO_DOMAIN_TR}#Argo-k0baya-Trojan" | base64 ) > sub
+trojan://${UUID}@upos-sz-mirrorcf1ov.bilivideo.com:443?path=%2F${WSPATH}-trojan%3Fed%3D2560&security=tls&host=\${ARGO_DOMAIN_TR}&type=ws&sni=\${ARGO_DOMAIN_TR}#Argo-Trojan" | base64 ) > sub
 
 }
 [ ! -e \${WORKDIR}/cloudflared ] && check_file
@@ -434,4 +434,4 @@ cd ${WORKDIR}
 generate_config
 generate_argo
 
-[ -e ${WORKDIR}/argo.sh ] && echo "请访问 https://${USERNAME_DOMAIN}.serv00.net/status 获取服务端状态, 当 cloudflared 与 web.js 正常运行后，访问 https://${USERNAME_DOMAIN}.serv00.net/list 获取配置"
+[ -e ${WORKDIR}/argo.sh ] && echo "Please visit https://${USERNAME_DOMAIN}.serv00.net/status Get server status, When cloudflared and web.js are running properly, visit https://${USERNAME_DOMAIN}.serv00.net/list Get Configuration"
